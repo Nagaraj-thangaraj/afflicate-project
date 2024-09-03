4f2bf3531b0f307d315f5c83f4c93948a31f93fe
NagarajWunderux <nagarajt@wunderux.com>
2024-09-03 22:02:31 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -8,25 +8,11 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware setup
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow these HTTP methods
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow these headers
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // Send 200 OK for OPTIONS requests
-  }
-
-  next();
-});
-
 app.use(express.json());
 app.use("/api", productRoutes);
 
 // Environment variables
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Check if MONGODB_URI is defined
@@ -47,7 +33,28 @@ mongoose
     console.error("Error connecting to MongoDB:", err);
     process.exit(1); // Exit the application if the connection fails
   });
+// Configure CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from your frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
+// Middleware setup
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow these HTTP methods
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow these headers
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Send 200 OK for OPTIONS requests
+  }
+
+  next();
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
