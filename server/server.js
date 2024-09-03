19c57a95b9d0ff7925a93f39b72af17f94161c2d
NagarajWunderux <nagarajt@wunderux.com>
2024-09-04 03:15:55 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173/", // Adjust this as needed
+    origin: "*", // Adjust this as needed
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
